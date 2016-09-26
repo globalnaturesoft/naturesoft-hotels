3836dc8d242609d13ca86ca8ba83d3e8d4b3dca0
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-26 00:10:42 -0700

    update feature: service of hotels engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Naturesoft::Hotels::Engine.routes.draw do
   namespace :admin, module: "admin", path: "admin/hotels" do
+    resources :services do
+      collection do
+        put "enable"
+        put "disable"
+        delete "delete"
+        get "select2"
+      end
+    end
     resources :room_types do
       collection do
         put "enable"
