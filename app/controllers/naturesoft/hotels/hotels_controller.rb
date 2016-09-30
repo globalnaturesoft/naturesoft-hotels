3c27d992dc1f800268b8b870bb5f5d9c8c27991c
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-09-30 00:29:11 -0700

    add controller hotel frontend

---
+++ app/controllers/naturesoft/hotels/hotels_controller.rb
@@ -0,0 +1,14 @@
+module Naturesoft
+  module Hotels
+    class HotelsController < Naturesoft::FrontendController
+      def hotels_grid
+      end
+      
+      def hotels_list
+      end
+      
+      def detail
+      end
+    end
+  end
+end
