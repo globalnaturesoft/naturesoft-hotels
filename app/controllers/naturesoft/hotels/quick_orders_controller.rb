9cd2f611c592f794b7783734bb210aef9b423591
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-10-25 00:19:45 -0700

    update module quick booking order

---
+++ app/controllers/naturesoft/hotels/quick_orders_controller.rb
@@ -0,0 +1,29 @@
+module Naturesoft
+  module Hotels
+    class QuickOrdersController < Naturesoft::FrontendController
+      before_action :set_quick_order, only: []
+      
+      # POST /quick_orders
+      def quick_booking
+        @quick_order = QuickOrder.new(quick_order_params)
+  
+        if @quick_order.save
+          render text: 'Quick order was successfully created.'
+        else
+          render text: 'Sorry!.'
+        end
+      end
+  
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_quick_order
+          @quick_order = QuickOrder.find(params[:id])
+        end
+  
+        # Only allow a trusted parameter "white list" through.
+        def quick_order_params
+          params.fetch(:quick_order, {}).permit(:first_name, :last_name, :email, :phone, :message)
+        end
+    end
+  end
+end
