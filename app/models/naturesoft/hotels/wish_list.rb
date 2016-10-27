0af0664b736a99a90440ac77c05b991f81e04057
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-10-27 02:46:35 -0700

    update

--- app/models/naturesoft/hotels/wish_list.rb
+++ app/models/naturesoft/hotels/wish_list.rb
@@ -2,5 +2,9 @@ module Naturesoft::Hotels
   class WishList < ApplicationRecord
     belongs_to :user
     belongs_to :hotel
+    
+    def self.check_present(hotel_id, user_id)
+      self.where(hotel_id: hotel_id).where(user_id: user_id)
+    end
   end
 end
